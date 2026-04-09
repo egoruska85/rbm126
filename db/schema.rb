ffea09b1578adc479bca0aa8f4146a22010a67d7
egoruska85 <egorarbuzov@gmail.com>
2026-04-09 13:09:33 +0300

    add position services

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_04_09_093816) do
+ActiveRecord::Schema[7.0].define(version: 2026_04_09_095852) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -100,6 +100,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_04_09_093816) do
     t.text "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "numeric"
   end
 
   create_table "subservices", force: :cascade do |t|
